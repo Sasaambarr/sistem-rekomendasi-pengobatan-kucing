0a19efe1195a59cadb0c97917f03097ceeb1596c
Sasaambarr <salsaambarwidyaningrum@gmail.com>
2026-07-06 23:12:54 +0700

    fix mysql railway connection

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from flask_cors import CORS
 from datetime import datetime
 import pytz
 import joblib
-import mysql.connector
 import os
+import mysql.connector
 
 db = mysql.connector.connect(
     host=os.getenv("MYSQLHOST"),
     user=os.getenv("MYSQLUSER"),
     password=os.getenv("MYSQLPASSWORD"),
     database=os.getenv("MYSQLDATABASE"),
-    port=int(os.getenv("MYSQLPORT"))
+    port=int(os.getenv("MYSQLPORT", 3306))
 )
 
 app = Flask(__name__)
